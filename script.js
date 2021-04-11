c5b2be4bea39b2249c7ff3f4680b9b27de5cb5f5
Lauren Kruse <laurenjmkruse@gmail.com>
2021-04-11 11:59:59 -0400

    Added 5-day forecast

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
     // *********************************************
     // A. Variables
     let cityName;
-    let queryURLforWeather;
+    let queryURLforCurrentWeather;
     let queryURLforUVIndex;
-    let returnedData;
+    let queryURLforForecast;
+    let returnedCurrentData;
+    let returnedForecastData;
     let currentLocation;
     let currentDate;
     let latitude;
@@ -24,13 +26,13 @@ $(document).ready(function() {
     // *********************************************
     // B. Functions
     
-    // 02. Initiate application
+    // 01. Initiate application
     function init() {
         $('#forecast-panel').css('visibility', 'hidden');
         console.log('Application initiated!');
     };
 
-    // 01. Event Listener for search button
+    // 02. Event Listener for search button
     $('#submit-button').on('click', function() {
         console.log('Submit button clicked!');
 
@@ -45,39 +47,40 @@ $(document).ready(function() {
         $('#location-input').val('');
     });
 
-    // 02. Creating row in history list
-    function createRow(placeName) {
-        let newCity = $('<li>').addClass('list-group-item list-group-item-action').text(placeName);
+    // 03. Creating row in history list
+    function createRow(historyCityName) {
+        let newCity = $('<li>').addClass('list-group-item list-group-item-action').text(historyCityName);
         historyList.append(newCity);
-        console.log(placeName + ` was added to the history list.`);  
+        console.log(historyCityName + ` was added to the history list.`);  
     };
   
-    // 03. Searching for location in API and gathering data 
+    // 04. Searching for location in API and gathering current data 
     function seekLocation(locationName) {
         cityName = locationName;
-        queryURLforWeather = 'http://api.openweathermap.org/data/2.5/weather?q=' + locationName + '&appid=5a8f172622a946ab1036f252f6d9db73&units=imperial'
+        queryURLforCurrentWeather = 'http://api.openweathermap.org/data/2.5/weather?q=' + locationName + '&appid=5a8f172622a946ab1036f252f6d9db73&units=imperial'
         $.ajax({
             type: 'GET',
-            url: queryURLforWeather,
+            url: queryURLforCurrentWeather,
             dataType: 'json',            
-        }).then (function(openWeatherMapData) {
-            returnedData = openWeatherMapData;
+        }).then (function(currentWeatherData) {
+            returnedCurrentData = currentWeatherData;
             
-            console.log(`Data for ${locationName} retrieved from Open Weather Map API.`);
+            console.log(`Current data for ${locationName} retrieved from Open Weather Map API.`);
 
-            console.log(openWeatherMapData);
+            console.log(currentWeatherData);
 
-            latitude = openWeatherMapData.coord.lat;
-            longitude = openWeatherMapData.coord.lon;
+            latitude = currentWeatherData.coord.lat;
+            longitude = currentWeatherData.coord.lon;
             console.log(`Latitude is ${latitude}; Longitude is ${longitude}`);
 
-            populateCurrentWeather(openWeatherMapData);
+            populateCurrentWeather(currentWeatherData);
             seekUVIndex(latitude, longitude);
+            seekForecast(cityName);
         });
-    };
+     };
 
-    // 04. Adding retrieved data for current weather
-    function populateCurrentWeather(returnedData) {
+    // 05. Adding retrieved data for current weather
+    function populateCurrentWeather(returnedCurrentData) {
         // Populate current city name
         $('#recent-locations').text('Recent Locations');
         currentLocation = $('#location-name').text(cityName);
@@ -86,26 +89,14 @@ $(document).ready(function() {
         currentDate = $('#current-date').text(new Date().toLocaleDateString());
         
         // Populate conditions
-        currentConditions = $('#present-conditions').text(returnedData.weather[0].description);
-        currentWeatherIcon = $('#weather-depiction').attr('src', 'http://openweathermap.org/img/w/' + returnedData.weather[0].icon + '.png');
-        currentTemperature = $('#present-temperature').text(`Temperature: ${returnedData.main.temp} °F`);
-        currentHumidity = $('#present-humidity').text(`Humidity: ${returnedData.main.humidity}%`);
-        currentWindSpeed = $('#present-wind-speed').text(`Wind Speed: ${returnedData.wind.speed} mph`);      
+        currentConditions = $('#present-conditions').text(returnedCurrentData.weather[0].description);
+        currentWeatherIcon = $('#weather-depiction').attr('src', 'http://openweathermap.org/img/w/' + returnedCurrentData.weather[0].icon + '.png');
+        currentTemperature = $('#present-temperature').text(`Temperature: ${returnedCurrentData.main.temp} °F`);
+        currentHumidity = $('#present-humidity').text(`Humidity: ${returnedCurrentData.main.humidity}%`);
+        currentWindSpeed = $('#present-wind-speed').text(`Wind Speed: ${returnedCurrentData.wind.speed} mph`);      
     };
 
-    // 05. Removing values from forecast panel to prepare for different location
-    function clearValues() {
-        currentLocation.val('');
-        currentDate.val('');
-        currentConditions.val('');
-        currentWeatherIcon.val('');
-        currentTemperature.val('');
-        currentHumidity.val('');
-        currentWindSpeed.val('');
-        currentUVIndex.val('');     
-    };
-
-    // 05. Obtaining and adding data for current UV Index
+    // 06. Obtaining and adding data for current UV Index
     function seekUVIndex(latd, long) {
         queryURLforUVIndex = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latd + '&lon=' + long  + '&appid=5a8f172622a946ab1036f252f6d9db73&units=imperial';
         
@@ -138,19 +129,65 @@ $(document).ready(function() {
         });
     };
 
-    // 06. Adding sought location to local storage
+    // 07. Gathering forecast data 
+    function seekForecast(placeName) {
+        queryURLforForecast = 'http://api.openweathermap.org/data/2.5/forecast?q=' + placeName + '&appid=5a8f172622a946ab1036f252f6d9db73&units=imperial'
+        $.ajax({
+            type: 'GET',
+            url: queryURLforForecast,
+            dataType: 'json',            
+        }).then (function(forecastData) {
+            returnedForecastData = forecastData;
+            
+            console.log(`Forecast data for ${placeName} retrieved.`);
 
+            console.log(returnedForecastData);
 
-    // 07. Seeking current conditions from city in search history
+            buildForecast(returnedForecastData);
+        });
+    };
+    
+    // 07. Assembling 5-day forecast
+    function buildForecast(returnedForecastData) {        
+        // Ensure forecast panel clear of any previous data
+        $('#predicted-conditions').empty();
+
+        for (let i = 0; i < returnedForecastData.list.length; i++) {
+            
+            // Seek forecast for next 5 days at noon; build and populate elements
+            if (returnedForecastData.list[i].dt_txt.indexOf('12:00:00') !== -1) {
+                let dayBlock = $('<div>').addClass('card inline-block future').css('margin-left', '15px');
+                let forecastDate = $('<h6>').addClass('card-title').text(new Date(returnedForecastData.list[i].dt_txt).toLocaleDateString());
+                let forecastIcon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + returnedForecastData.list[i].weather[0].icon + '.png');
+                let forecastTemperature = $('<p>').addClass('card-text').text(`Temperature: ${returnedForecastData.list[i].main.temp} °F`);
+                let forecastHumidity = $('<p>').addClass('card-text').text(`Humidity: ${returnedForecastData.list[i].main.humidity}%`);
+            
+                dayBlock.append(forecastDate, forecastIcon, forecastTemperature, forecastHumidity);
+                $('#predicted-conditions').append(dayBlock);
+            }            
+        }
+    };
+
+    // // 08. Removing values from forecast panel to prepare for different location
+    // function clearValues() {
+    //     currentLocation.val('');
+    //     currentDate.val('');
+    //     currentConditions.val('');
+    //     currentWeatherIcon.val('');
+    //     currentTemperature.val('');
+    //     currentHumidity.val('');
+    //     currentWindSpeed.val('');
+    //     currentUVIndex.val('');     
+    // };
+
+    // 09. Seeking current conditions from city in search history
     $('#search-history').on('click', 'li', function() {
         seekLocation ($(this).text());
         console.log(`Revisiting ${$(this).text()} from history list.`);
-        //currentLocation = $('#location-name').text(cityName);
-
     });
 
-    // Initial process
+
+    // *********************************************
+    // C. Initial process
     init();
-
-
 });
